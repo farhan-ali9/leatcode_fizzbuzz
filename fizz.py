390978a0dfdc8593942054c10e720ffdfb9ab45a
Farhan Ali <80478326+farhan-ali9@users.noreply.github.com>
2023-08-04 23:35:17 +0500

    Create fizz.py

---
+++ fizz.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def fizzBuzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        ans=[]
+        i=1
+        while i<=n:
+            if i%3==0 and i%5==0:
+                ans.append("FizzBuzz")
+            elif i%3==0:
+                    ans.append("Fizz")
+            elif i%5==0:
+                ans.append("Buzz")
+            else:
+                ans.append(str(i))
+            i+=1
+        return ans
+            
